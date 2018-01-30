6a044aa12f45b23a4f7fb26044d60d857dda5528
fanliyong007 <fanliyong007@hotmail.com>
2018-01-30 10:31:17 +0800

    ok
    20190130

---
+++ src/BinarySearch.java
@@ -0,0 +1,64 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BinarySearch
+{
+    public  static int rank(int key,int[] a)
+    {
+        int right=0;
+        int left=a.length-1;
+        while (right<left)
+        {
+            int mid=right+(left-right)/2;
+            if(key<a[mid])
+                left=mid-1;
+            else if (key>a[mid])
+                right=mid+1;
+            else
+                return mid;
+//            while(a[mid]==key)
+//            {
+//                mid-=1;
+//            }
+//            return mid
+
+        }
+        return -1;
+    }
+    public static int count(int key,int[] a)
+    {
+        int right=0;
+        int left=a.length-1;
+        while (right<left)
+        {
+            int mid=right+(left-right)/2;
+            int num=0;
+            if(key<a[mid])
+                left=mid-1;
+            else if (key>a[mid])
+                right=mid+1;
+            else
+            {
+                while(a[mid]==key)
+                {
+                    num++;
+                    mid-=1;
+                }
+                return num;
+            }
+
+        }
+        return -1;
+    }
+    public static void main(String args[])
+    {
+        int[] whitelist=In.readInts();
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty())
+        {
+            int key=StdIn.readInt();
+            if(rank(key,whitelist)<0)
+                StdOut.println(key);
+        }
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test
+{
+    public static void main(String args[])
+    {
+        StdDraw.line(0,0,1,1);
+    }
+}
